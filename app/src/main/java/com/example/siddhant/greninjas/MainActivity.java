9f6fe1567ebb9982bf0f30673edb79209f0c23e4
Siddhant Bajaj <sbajaj1997@gmail.com>
2016-07-26 03:27:19 +0530

    MainLayout

--- app/src/main/java/com/example/siddhant/greninjas/MainActivity.java
+++ app/src/main/java/com/example/siddhant/greninjas/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.siddhant.greninjas;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,25 +29,151 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
+    private ViewPager mViewPager;
+    TabLayout tabLayout;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab,fab1,fab2;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    NavigationView navigationView;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int SELECT_PICTURE = 100;
+    Uri selectedImageUri;
+    TextView name;
+    android.support.v7.widget.AppCompatImageView userImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Shared prefrence
+       final SharedPreferences sp=getSharedPreferences("Gre Ninjas", Context.MODE_PRIVATE);
+       final SharedPreferences.Editor editor=sp.edit();
+        //Set Username and userImage
+        navigationView=(NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        name=(TextView)headerView.findViewById(R.id.name);
+        name.setText(sp.getString("username",null));
+        userImage=(AppCompatImageView) headerView.findViewById(R.id.Userimage);
+        if(sp.getString("imageUri",null)!=null)
+        {
+            userImage.setImageURI(Uri.parse(sp.getString("imageUri",null)));
+        }
+        Boolean isFirst=sp.getBoolean("isFirst",true);
+        if(isFirst==true)
+        {
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("LOGIN");
+           final View v= getLayoutInflater().inflate(R.layout.login_dialog,null);
+            builder.setView(v);
+            builder.setCancelable(false);
+            builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    EditText username=(EditText) v.findViewById(R.id.username);
+                    EditText password=(EditText) v.findViewById(R.id.password);
+                    editor.putBoolean("isFirst",false);
+                    editor.putString("username",String.valueOf(username.getText()));
+                    editor.putString("password",String.valueOf(password.getText()));
+                    editor.commit();
+                    name.setText(String.valueOf(username.getText()));
+                    dialog.dismiss();
+                }
+            });
+            builder.create().show();
+        }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        userImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                final AlertDialog builder = new AlertDialog.Builder(MainActivity.this).create();
+                builder.setTitle("ADD PHOTO");
+                final View v1= getLayoutInflater().inflate(R.layout.user_image_dialog,null);
+
+                builder.setView(v1);
+                TextView choose=(TextView)v1.findViewById(R.id.choose);
+                TextView camera=(TextView)v1.findViewById(R.id.takePhoto);
+
+                choose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent();intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+                        builder.dismiss();
+
+
+                    }
+                });
+                camera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                        builder.dismiss();
+                    }
+                });
+
+                builder.show();
             }
         });
+        ///tabs
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+       mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        ///SET TAB ICONS
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.white));
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_library_books_white_24dp);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_chrome_reader_mode_white_24dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_group_add_white_24dp);
+
+       // FloatingActionButton Animation
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateFAB();
+            }
+        });
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Raj", "Fab 1");
+            }
+        });
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Raj", "Fab 2");
+            }
+        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +184,31 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+    public void animateFAB(){
+
+        if(isFabOpen){
+
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+            Log.d("Raj", "close");
+
+        } else {
+
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+            Log.d("Raj","open");
+
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -50,6 +218,52 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final SharedPreferences sp=getSharedPreferences("Gre Ninjas", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor=sp.edit();
+        if (resultCode == RESULT_OK) {if (requestCode == SELECT_PICTURE) {// Get the url from
+            Toast.makeText(MainActivity.this,"Image set",Toast.LENGTH_LONG).show();
+            selectedImageUri = data.getData();
+            userImage.setImageURI(selectedImageUri);
+            editor.putString("imageUri",selectedImageUri.toString());
+            editor.commit();
+
+
+
+        }}
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Toast.makeText(MainActivity.this, "Image set", Toast.LENGTH_LONG).show();
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            userImage.setImageBitmap(photo);
+
+       Uri test=getImageUri(MainActivity.this,photo);
+            editor.putString("imageUri",test.toString());
+            editor.commit();
+
+
+
+        }}
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+    public String getPathFromURI(Uri contentUri)
+    {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst())
+        {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
     }
 
     @Override
@@ -90,12 +304,52 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    return new BlankFragment();
+
+
+                case 1:
+                    return new BlankFragment();
+
+
+
+                case 2:
+                    return new BlankFragment();
+
+                default:
+                    return null;
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            return null;
+        }
+    }
 }
+
---
+++ app/src/main/java/com/example/siddhant/greninjas/SectionsPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.siddhant.greninjas;
+
+/**
+ * Created by ABC on 25-07-2016.
+ */
+public class SectionsPagerAdapter {
+}
